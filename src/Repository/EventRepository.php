b3fc5d5f4484eaea5037ce720947cc522a7e7b78
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-02-23 16:38:19 +0100

    ajout methode getAllEventByType

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Event;
+use App\Entity\TypeEvent;
 use App\Entity\Veterinaire;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
