f50077eb74682ca588326341e253c73c67f278ec
Nicolas Mossmann <nicolas.mossmann@etudiant.univ-reims.fr>
2023-02-25 23:53:45 +0100

    Remove useless import

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,8 +2,6 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use App\Repository\UserRepository;
