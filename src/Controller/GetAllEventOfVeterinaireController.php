7330dee079f20442bce44a81d994d062792edee6
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-02-26 17:36:36 +0100

    Ajout controller dans l'operation Veterinaire pour utiliser l'id a la place de l'utilisateur connecter

--- src/Controller/GetAllEventOfVeterinaireController.php
+++ src/Controller/GetAllEventOfVeterinaireController.php
@@ -3,14 +3,23 @@
 namespace App\Controller;
 
 use App\Repository\EventRepository;
+use App\Repository\VeterinaireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GetAllEventOfVeterinaireController extends AbstractController
 {
-    public function __invoke(EventRepository $er): array
+    private EventRepository $er;
+    private VeterinaireRepository $vr;
+
+    public function __construct(EventRepository $er, VeterinaireRepository $vr)
     {
-        return $er->getAllEventByVeterinaire($this->getUser());
+        $this->er = $er;
+        $this->vr = $vr;
+    }
+    public function __invoke(int $id): array
+    {
+        return $this->er->getAllEventByVeterinaire($this->vr->find($id));
     }
 }
