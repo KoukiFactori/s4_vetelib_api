11e6a93d3ad1d9141c667f9a0b499250133cf094
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-02-26 16:10:03 +0100

    AJOUT QUERYSTRING pour recuperer tout les events d'un type a la place d'une route | modification securité pour pallier au erreurs | modification controlleur pour palier a des probleme | ajout operation getCollection en fonction du type dans l'apiRessource d'un veterinaire

--- src/Controller/GetAllEventOfClientController.php
+++ src/Controller/GetAllEventOfClientController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -9,8 +10,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class GetAllEventOfClientController extends AbstractController
 {
-    public function __invoke(EventRepository $er): array
+    public function __invoke(EventRepository $er , int $id): array
     {
-        return $er->getAllEventByClient($this->getUser());
+        return $er->getAllEventByClient($this->getDoctrine()->getRepository(User::class)->find($id));
     }
 }
