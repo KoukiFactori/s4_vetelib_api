f481471ed506a46c24b7e0fc20359fa2bd5539df
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-02-26 17:33:14 +0100

    modification controlleur pour respecter symfony

--- src/Controller/GetAllEventOfClientController.php
+++ src/Controller/GetAllEventOfClientController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\EventRepository;
 use App\Repository\UserRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
