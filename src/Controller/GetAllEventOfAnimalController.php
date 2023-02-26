9a4996b036c92fc500eb0fe153621860ffa0d7d6
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-02-26 17:28:04 +0100

    Modification operation sur la ressource animal pour autoriser seulement l'admin et le proprietaire de l'animal passer a recuperer tout les events de cet animal

---
+++ src/Controller/GetAllEventOfAnimalController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\AnimalRepository;
+use App\Repository\EventRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class GetAllEventOfAnimalController extends AbstractController
+{
+    private EventRepository $er;
+    private AnimalRepository $ar;
+
+    public function __construct(EventRepository $er, AnimalRepository $ar)
+    {
+        $this->er = $er;
+        $this->ar = $ar;
+    }
+    public function __invoke(int $id):array
+    {   
+        $events =[];
+        if  ($this->isGranted("ROLE_ADMIN")) {
+            $events=$this->er->findEventByAnimal($this->ar->find($id));
+        }
+
+       if ($this->isGranted("ROLE_CLIENT")) {
+            $animal=$this->ar->find($id);
+            if ($animal->getClient()->getId() == $this->getUser()->getId()) {
+                $events= $this->er->findEventByAnimal($animal);
+            } else {
+                throw new AccessDeniedException("Vous n'avez pas accès à cet animal");
+            }
+       }
+         return $events;
+    }
+}
