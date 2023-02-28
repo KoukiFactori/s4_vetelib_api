89e23b574ac08d280c6e5e253e0fe6c56f2eccd2
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-03-01 00:31:31 +0100

    Ajout test AnonymousUserCannotGetEvent

--- tests/Api/Event/EventGetCest.php
+++ tests/Api/Event/EventGetCest.php
@@ -4,10 +4,18 @@ namespace App\Tests\Api\Event;
 
 use App\Entity\Event;
 use App\Factory\EventFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ApiTester;
 use Codeception\Util\HttpCode;
 
 class UserGetMeCest
 {
-    
+    public function anonymousUserCannotGetEvent(ApiTester $I): void
+    {
+        EventFactory::createOne();
+        $I->sendGET('/api/events/1');
+        $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
+    }
+
+
 }
