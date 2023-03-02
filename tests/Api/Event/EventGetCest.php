ae9c4dc9690b2131eaa50d8cbfa02c81794bc79d
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-03-02 22:28:27 +0100

    Correction Erreur pour lancer les test | Ajout methode private pour generer des données

--- tests/Api/Event/EventGetCest.php
+++ tests/Api/Event/EventGetCest.php
@@ -3,6 +3,9 @@
 namespace App\Tests\Api\Event;
 
 use App\Entity\Event;
+use App\Factory\AnimalFactory;
+use App\Factory\ClientFactory;
+use App\Factory\EspeceFactory;
 use App\Factory\EventFactory;
 use App\Factory\UserFactory;
 use App\Tests\Support\ApiTester;
@@ -10,8 +13,27 @@ use Codeception\Util\HttpCode;
 
 class UserGetMeCest
 {
+    private function InitialiseData(){
+        $veterinaire = UserFactory::createOne();
+        $client = UserFactory::createOne();
+        $espece=EspeceFactory::createOne();
+        $animal = AnimalFactory::createOne(
+            [   'espece' => $espece,
+                'client' => $client
+            ]
+        );
+        $event = EventFactory::createOne(
+            [
+                'veterinaire' => $veterinaire,
+                'animal' => $animal
+            ]
+        );
+
+    }
     public function anonymousUserCannotGetEvent(ApiTester $I): void
-    {
+    {   
+        AnimalFactory::createOne();
+        ClientFactory::createOne();
         EventFactory::createOne();
         $I->sendGET('/api/events/1');
         $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
@@ -24,10 +46,59 @@ class UserGetMeCest
         $veterinaire2 = UserFactory::createOne();
         EventFactory::createOne(
             [
-                'veterinaire' =>'/api/veterinaires/'.$veterinaire2->getId(),
+                'veterinaire' => $veterinaire2
             ]
         );
-        $I->sendGET('/api/events/1');
+        $I->sendGET('/api/veterinaires/2/events');
         $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
     }
+    public function AuthenticatedVeterinaireCanGetHisEvent(ApiTester $I): void
+    {
+        $veterinaire = UserFactory::createOne();
+        $I->amAuthenticatedAs($veterinaire->object());
+        EventFactory::createOne(
+            [
+                'veterinaire' => $veterinaire
+            ]
+        );
+        $I->sendGET('/api/veterinaires/1/events');
+        $I->seeResponseCodeIs(HttpCode::OK);
+    }
+
+    public function AuthenticatedClientCantGetOtherClientEvent(ApiTester $I): void
+    {
+        $user = UserFactory::createOne();
+        $I->amAuthenticatedAs($user->object());
+        $user2 = UserFactory::createOne();
+        AnimalFactory::createOne(
+            [
+                'client' =>$user2
+            ]
+        );
+        EventFactory::createOne(
+            [
+                'animal' => '/api/animals/1'
+            ]
+        );
+        $I->sendGET('/api/clients/2/events');
+        $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
+    }
+    public function AuthenticatedClientCanGetHisEvent(ApiTester $I): void
+    {
+        $user = UserFactory::createOne();
+        $I->amAuthenticatedAs($user->object());
+        $animal = AnimalFactory::createOne(
+            [
+                'client' =>$user
+            ]
+        );
+        EventFactory::createOne(
+            [
+                'animal' => $animal
+            ]
+        );
+        $I->sendGET('/api/clients/1/events');
+        $I->seeResponseCodeIs(HttpCode::OK);
+    }
+
 }
--- tests/Support/Module/ApiPlatform.php
+++ tests/Support/Module/ApiPlatform.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace App\Tests\Support\Module;
 
-use Codeception\Exception\ModuleException;
 use Codeception\Module\REST;
-use Codeception\Module\Symfony;
-use Codeception\TestInterface;
 use Codeception\Util\JsonType;
-use Doctrine\Persistence\ManagerRegistry;
 
 class ApiPlatform extends REST
 {
@@ -27,27 +23,6 @@ class ApiPlatform extends REST
         JsonType::addCustomFilter('path', function ($value) {
             return preg_match('@^'.$this->getBasePath().'(?:/\w+)*$@', $value);
         });
-    }
-
-    /**
-     * Executed before tests.
-     *
-     * Fix "The EntityManager is closed" after test failure. If EntityManager is closed, reset it.
-     */
-    public function _before(TestInterface $test): void
-    {
-        parent::_before($test);
-
-        try {
-            /** @var Symfony $symfonyModule */
-            $symfonyModule = $this->getModule('Symfony');
-            /** @var ManagerRegistry $doctrine */
-            $doctrine = $symfonyModule->grabService('doctrine');
-            if (!$doctrine->getManager()->isOpen()) {
-                $doctrine->resetManager();
-            }
-        } catch (ModuleException) {
-        }
     }
 
     /**
