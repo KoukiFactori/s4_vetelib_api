7fa6798043802d4b8d352426c0b23ab46ed606f9
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-03-03 22:02:55 +0100

    Override methode start pour renvoyer un code erreur et pas une redirection quand pas connecter

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -61,6 +61,8 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         return new RedirectResponse($url);
     }
 
+        return new RedirectResponse($url);
+    }
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
