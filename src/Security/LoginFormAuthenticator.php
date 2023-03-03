e9dbfdf20022e79fdc73ca4428c229a53e61eb7a
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-03-03 22:02:55 +0100

    Override methode start pour renvoyer un code erreur et pas une redirection quand pas connecter

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -5,8 +5,10 @@ namespace App\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
@@ -39,7 +41,26 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
             ]
         );
     }
+      /**
+     * Override to control what happens when the user hits a secure page
+     * but isn't logged in yet.
+     *
+     * @return RedirectResponse
+     *
+     * @throws HttpException
+     */
+    public function start(Request $request, AuthenticationException $authException = null): RedirectResponse
+    {
+        /** @var string $route current route */
+        $route = $request->get('_route');
+        // API route ?
+        if (str_starts_with($route, '_api_')) {
+            throw new HttpException(Response::HTTP_UNAUTHORIZED);
+        }
+        $url = $this->getLoginUrl($request);
 
+        return new RedirectResponse($url);
+    }
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
