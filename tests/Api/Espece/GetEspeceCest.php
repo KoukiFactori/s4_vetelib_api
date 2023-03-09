57b754ca9e2983d5c8b04ffaac908329dc76aaff
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-03-09 08:59:18 +0100

    Génération de la suite de tests

---
+++ tests/Api/Espece/GetEspeceCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Api;
+
+use App\Tests\Support\ApiTester;
+
+class GetEspeceCest
+{
+    public function _before(ApiTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ApiTester $I)
+    {
+    }
+}
