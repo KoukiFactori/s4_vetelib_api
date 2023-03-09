31ffa2f61e419466026cf947e1a7180a989e86e2
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-03-09 08:49:43 +0100

    Oublie des imports

--- src/Entity/Espece.php
+++ src/Entity/Espece.php
@@ -3,8 +3,10 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
 use App\Repository\EspeceRepository;
 use Doctrine\ORM\Mapping as ORM;
