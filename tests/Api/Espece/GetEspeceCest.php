39b2c61e15701c13318933bbe368f6e6999cfd0d
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-03-09 09:07:17 +0100

    Création du test "authenticatedClientCanGetEspece"

--- tests/Api/Espece/GetEspeceCest.php
+++ tests/Api/Espece/GetEspeceCest.php
@@ -1,18 +1,19 @@
 <?php
 
+namespace App\Tests\Api\Espece;
 
-namespace App\Tests\Api;
-
+use App\Factory\ClientFactory;
+use App\Factory\EspeceFactory;
 use App\Tests\Support\ApiTester;
 
 class GetEspeceCest
 {
-    public function _before(ApiTester $I)
+    public function authenticatedClientCanGetEspece(ApiTester $I): void
     {
-    }
-
-    // tests
-    public function tryToTest(ApiTester $I)
-    {
+        EspeceFactory::createOne();
+        $user = ClientFactory::createOne();
+        $I->amLoggedInAs($user->object());
+        $I->sendGET('/api/especes/1');
+        $I->seeResponseCodeIs(200);
     }
 }
