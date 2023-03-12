b7c2e38365eb6ae17a306c39f30076de99659db0
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-03-12 15:33:07 +0100

    ajout config pour que mes ifs fonctionne dans mes test ( merci monsieur cutrona )

--- src/EntityListener/EventAnimalIsOwnByAuthenticatedClientListener.php
+++ src/EntityListener/EventAnimalIsOwnByAuthenticatedClientListener.php
@@ -26,19 +26,19 @@ class EventAnimalIsOwnByAuthenticatedClientListener
         $this->em = $em;
     }
     public function prePersist(Event $event): void
-    {   
+    {     
+        
         if ($this->security->isGranted('ROLE_CLIENT')) {
-            dd('ici client 1');
             if ($this->security->getUser()->getId() != $event->getAnimal()->getClient()->getId()) {
-                dd('ici client 2');
+
                 throw new AccessDeniedException("Vous ne pouvez pas creer ce rendez-vous , vous n'etes pas le propriétaire de l'animal");
             }
         }
 
         if ($this->security->isGranted('ROLE_VETERINAIRE')) {
-            dd('ici veterinaire 1');
+
             if ($this->security->getUser()->getId() != $event->getVeterinaire()->getId()) {
-                dd('ici veterinaire 2');
+ 
                 throw new AccessDeniedException("Vous ne pouvez pas creer ce rendez-vous , vous n'etes pas le veterinaire concerné");
             }
         }
