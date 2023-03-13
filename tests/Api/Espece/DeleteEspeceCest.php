38ddb4aa7d3bc8a605de38629e3442639322a4b5
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-03-13 14:51:22 +0100

    Ajout des tests vérifiant que seul l'admin peut delete des especes

--- tests/Api/Espece/DeleteEspeceCest.php
+++ tests/Api/Espece/DeleteEspeceCest.php
@@ -3,7 +3,9 @@
 namespace App\Tests\Api\Espece;
 
 use App\Factory\AdminFactory;
+use App\Factory\ClientFactory;
 use App\Factory\EspeceFactory;
+use App\Factory\VeterinaireFactory;
 use App\Tests\Support\ApiTester;
 
 class DeleteEspeceCest
@@ -23,4 +25,23 @@ class DeleteEspeceCest
         $I->sendDELETE('/api/especes/1');
         $I->seeResponseCodeIs(204);
     }
+
+    public function authenticatedClientCannotDeleteEspece(ApiTester $I): void
+    {
+        $user = ClientFactory::createOne();
+        EspeceFactory::createOne();
+        $I->amLoggedInAs($user->object());
+        $I->sendDELETE('/api/especes/1');
+        $I->seeResponseCodeIs(403);
+    }
+
+    public function authenticatedVeterinaireCannotDeleteEspece(ApiTester $I): void
+    {
+        $user = VeterinaireFactory::createOne();
+        EspeceFactory::createOne();
+        $I->amLoggedInAs($user->object());
+        $I->sendDELETE('/api/especes/1');
+        $I->seeResponseCodeIs(403);
+    }
+
 }
