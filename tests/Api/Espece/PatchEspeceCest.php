acb6528698b64ac653738d6ab852833b5a011c1f
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-03-13 14:50:09 +0100

    Ajout des tests vérifiant que seul l'admin peut Patch une espece

--- tests/Api/Espece/PatchEspeceCest.php
+++ tests/Api/Espece/PatchEspeceCest.php
@@ -2,7 +2,10 @@
 
 namespace App\Tests\Api\Espece;
 
+use App\Factory\AdminFactory;
+use App\Factory\ClientFactory;
 use App\Factory\EspeceFactory;
+use App\Factory\VeterinaireFactory;
 use App\Tests\Support\ApiTester;
 
 class PatchEspeceCest
@@ -14,6 +17,39 @@ class PatchEspeceCest
             'name' => 'test',
         ]);
         $I->seeResponseCodeIs(401);
+    }
+
+    public function authenticatedAdminCanPatchEspece(ApiTester $I): void
+    {
+        $user = AdminFactory::createOne();
+        EspeceFactory::createOne();
+        $I->amLoggedInAs($user->object());
+        $I->sendPATCH('/api/especes/1', [
+            'name' => 'test',
+        ]);
+        $I->seeResponseCodeIs(200);
+    }
+
+    public function authenticatedClientCannotPatchEspece(ApiTester $I): void
+    {
+        $user = ClientFactory::createOne();
+        EspeceFactory::createOne();
+        $I->amLoggedInAs($user->object());
+        $I->sendPATCH('/api/especes/1', [
+            'name' => 'test',
+        ]);
+        $I->seeResponseCodeIs(403);
+    }
+
+    public function authenticatedVeterinaireCannotPatchEspece(ApiTester $I): void
+    {
+        $user = VeterinaireFactory::createOne();
+        EspeceFactory::createOne();
+        $I->amLoggedInAs($user->object());
+        $I->sendPATCH('/api/especes/1', [
+            'name' => 'test',
+        ]);
+        $I->seeResponseCodeIs(403);
     }
 
 }
