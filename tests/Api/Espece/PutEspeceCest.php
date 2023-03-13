85a00545ebe369d0435c85c7f7263e7557a5c912
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-03-13 14:45:35 +0100

    Ajout du test "authenticatedAdminCanPutEspece"

--- tests/Api/Espece/PutEspeceCest.php
+++ tests/Api/Espece/PutEspeceCest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Api\Espece;
 
+use App\Factory\AdminFactory;
 use App\Factory\EspeceFactory;
 use App\Tests\Support\ApiTester;
 
@@ -16,4 +17,14 @@ class PutEspeceCest
         $I->seeResponseCodeIs(401);
     }
 
+    public function authenticatedAdminCanPutEspece(ApiTester $I): void
+    {
+        $user = AdminFactory::createOne();
+        EspeceFactory::createOne();
+        $I->amLoggedInAs($user->object());
+        $I->sendPUT('/api/especes/1', [
+            'name' => 'test',
+        ]);
+        $I->seeResponseCodeIs(200);
+    }
 }
