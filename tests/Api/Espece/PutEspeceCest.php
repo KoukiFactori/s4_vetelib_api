39094f9536562adbbece69e556af66bf7c95928b
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-03-13 14:44:02 +0100

    Ajout du test "anonymousUserCannotPutEspece"

--- tests/Api/Espece/PutEspeceCest.php
+++ tests/Api/Espece/PutEspeceCest.php
@@ -2,9 +2,18 @@
 
 namespace App\Tests\Api\Espece;
 
+use App\Factory\EspeceFactory;
 use App\Tests\Support\ApiTester;
 
 class PutEspeceCest
 {
-    
+    public function anonymousUserCannotPutEspece(ApiTester $I): void
+    {
+        EspeceFactory::createOne();
+        $I->sendPUT('/api/especes/1', [
+            'name' => 'test',
+        ]);
+        $I->seeResponseCodeIs(401);
+    }
+
 }
