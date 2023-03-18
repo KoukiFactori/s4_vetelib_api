c60a4ed5bff4c6b40bd351b78f8e917318b74c3b
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-03-18 10:53:42 +0100

    Ajout classe de test
    Ajout classe de test pour tester la creation de TypeEvent

---
+++ tests/Api/TypeEvent/CreateTypeEventCest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Tests\Api\TypeEvent;
+use App\Factory\AdminFactory;
+use App\Factory\ClientFactory;
+use App\Factory\VeterinaireFactory;
+use App\Tests\Support\ApiTester;
+
+Class CreateTypeEventCest
+{
+
+}
