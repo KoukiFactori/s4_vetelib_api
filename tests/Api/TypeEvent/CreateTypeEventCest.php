39cad236f013e1e52bc5bbe29236e568572e2d2c
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-03-18 10:55:02 +0100

    Ajout test unitaire pour la création d'un type d'événement

--- tests/Api/TypeEvent/CreateTypeEventCest.php
+++ tests/Api/TypeEvent/CreateTypeEventCest.php
@@ -8,5 +8,13 @@ use App\Tests\Support\ApiTester;
 
 Class CreateTypeEventCest
 {
+    public function anonymousUserCannotCreateTypeEvent(ApiTester $I): void
+    {
+        $I->sendPOST('/api/type_events', [
+            'name' => 'test',
+        ]);
+        $I->seeResponseCodeIs(401);
+    }
+
 
 }
