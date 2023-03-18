bad5c069c7ad6ce8fe775af064a4a0659e882d2f
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-03-18 11:04:14 +0100

    Ajout test unitaire
    Ajout test unitaire pour la récupération d'un type d'événement par un utilisateur non connecté

--- tests/Api/TypeEvent/GetTypeEventCest.php
+++ tests/Api/TypeEvent/GetTypeEventCest.php
@@ -1,9 +1,16 @@
 <?php
 
 namespace App\Tests\Api\TypeEvent;
+use App\Tests\Support\ApiTester;
+use Codeception\Util\HttpCode;
 
 class GetTypeEventCest 
 {
+    public function anonymousUserCannotGetTypeEvent(ApiTester $I): void
+    {
+        $I->sendGET('/api/type_events');
+        $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
+    }
     
 }
 
