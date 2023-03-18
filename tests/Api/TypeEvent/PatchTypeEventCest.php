7b996fa87ea91d05937689b21c1284dec449a9be
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-03-18 12:33:30 +0100

    Ajout test unitaire
    Ajout test unitaire qui verifie qu'un user anonyme ne peut pas patch un type event

--- tests/Api/TypeEvent/PatchTypeEventCest.php
+++ tests/Api/TypeEvent/PatchTypeEventCest.php
@@ -1,8 +1,17 @@
 <?php
 
 namespace App\Tests\Api\TypeEvent;
+use App\Factory\TypeEventFactory;
+use App\Tests\Support\ApiTester;
 
 class PatchTypeEventCest
 {
-    
+    public function anonymousUserCannotPatchTypeEvent(ApiTester $I): void
+    {
+        TypeEventFactory::createOne();
+        $I->sendPATCH('/api/typeEvents/1', [
+            'name' => 'test',
+        ]);
+        $I->seeResponseCodeIs(401);
+    }
 }
