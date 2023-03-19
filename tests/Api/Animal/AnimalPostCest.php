1a8ecb3158bcafc79cc2b741c608f105533cc293
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-03-19 12:40:47 +0100

    Ajout de tests permettant de vérifier : - Qu'un client peut créer un animal pour lui même - Qu'un client ne peut pas créer d'animal pour un autre client

--- tests/Api/Animal/AnimalPostCest.php
+++ tests/Api/Animal/AnimalPostCest.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Tests\Api\Animal;
+use App\Factory\ClientFactory;
+use App\Factory\EspeceFactory;
 use App\Factory\VeterinaireFactory;
 use App\Tests\Support\ApiTester;
 use Codeception\Util\HttpCode;
@@ -25,6 +27,35 @@ class AnimalPostCest
             'name' => 'Donald',
             'espece' => '/api/especes/1',
             'client' => '/api/clients/1',
+        ]);
+        $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
+    }
+
+    public function authenticatedClientCanPostAnimalForHim(ApiTester $I): void
+    {
+        $user = ClientFactory::createOne();
+        EspeceFactory::createOne();
+        $I->amLoggedInAs($user->object());
+        $I->sendPOST('/api/animals', [
+            'name' => 'Donald',
+            'espece' => '/api/especes/1',
+            'client' => '/api/clients/1',
+            'birthdate' => '2021-01-01',
+        ]);
+        $I->seeResponseCodeIs(HttpCode::CREATED);
+    }
+
+    public function authenticatedClientCannotPostAnimalForOtherClient(ApiTester $I): void
+    {
+        $user = ClientFactory::createOne();
+        EspeceFactory::createOne();
+        ClientFactory::createOne();
+        $I->amLoggedInAs($user->object());
+        $I->sendPOST('/api/animals', [
+            'name' => 'Donald',
+            'espece' => '/api/especes/1',
+            'client' => '/api/clients/2',
+            'birthdate' => '2021-01-01',
         ]);
         $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
     }
