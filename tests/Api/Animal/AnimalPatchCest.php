fc60c766587ad004c066f74a7182de9214cb96fc
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-03-19 16:20:22 +0100

    Ajout de tests pour vérifier : - Qu'un client ne peut pas modifier les infos d'un animal qui ne lui appartient pas - Qu'un client ne peut pas modifier l'id client d'un animal

--- tests/Api/Animal/AnimalPatchCest.php
+++ tests/Api/Animal/AnimalPatchCest.php
@@ -9,6 +9,8 @@ use App\Factory\EspeceFactory;
 use App\Factory\VeterinaireFactory;
 use App\Tests\Support\ApiTester;
 use Codeception\Util\HttpCode;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
 
 class AnimalPatchCest
 {
@@ -52,17 +54,41 @@ class AnimalPatchCest
         $I->seeResponseCodeIs(HttpCode::OK);
     }
 
-    public function authenticatedClientCannotPatchAnimal(ApiTester $I): void
+    public function authenticatedClientCanPatchHisAnimal(ApiTester $I): void
     {
         $user = ClientFactory::createOne();
-        EspeceFactory::createOne();
-        AnimalFactory::createOne(['name' => 'Matoufeu', 'espece' => EspeceFactory::createOne(['name' => 'Chien']), 'client' => ClientFactory::createOne()]);
+        AnimalFactory::createOne(['name' => 'Matoufeu', 'espece' => EspeceFactory::createOne(['name' => 'Chien']), 'client' => $user]);
         $I->amLoggedInAs($user->object());
         $I->sendPATCH('/api/animals/1', [
             'name' => 'Donald',
             'espece' => '/api/especes/1',
             'client' => '/api/clients/1',
-            'birthdate' => '2021-01-01',
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+    }
+
+    public function authenticatedClientCannotPatchOtherClientAnimal(ApiTester $I): void
+    {
+        $user = ClientFactory::createOne();
+        $I->amLoggedInAs($user->object());
+        AnimalFactory::createOne(['name' => 'Matoufeu', 'espece' => EspeceFactory::createOne(['name' => 'Chien']), 'client' => ClientFactory::createOne()]);
+        $I->sendPATCH('/api/animals/1', [
+            'name' => 'Donald',
+            'espece' => '/api/especes/1',
+            'client' => '/api/clients/2',
+        ]);
+        $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
+    }
+
+    public function authenticatedClientCannotPatchDataClientAnimal(ApiTester $I): void
+    {
+        $user = ClientFactory::createOne();
+        $I->amLoggedInAs($user->object());
+        AnimalFactory::createOne(['name' => 'Matoufeu', 'espece' => EspeceFactory::createOne(['name' => 'Chien']), 'client' => ClientFactory::createOne()]);
+        $I->sendPATCH('/api/animals/1', [
+            'name' => 'Donald',
+            'espece' => '/api/especes/1',
+            'client' => '/api/clients/2',
         ]);
         $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
     }
