18951851b44e196ac80b58f80ef5aa5a001660ff
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-03-19 14:01:36 +0100

    Ajout du test pour vérifier qu'un admin peut post un animal

--- tests/Api/Animal/AnimalPostCest.php
+++ tests/Api/Animal/AnimalPostCest.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Tests\Api\Animal;
+
+use App\Factory\AdminFactory;
 use App\Factory\ClientFactory;
 use App\Factory\EspeceFactory;
 use App\Factory\VeterinaireFactory;
@@ -29,6 +31,21 @@ class AnimalPostCest
             'client' => '/api/clients/1',
         ]);
         $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
+    }
+
+    public function authenticatedAdminCanPostAnimal(ApiTester $I): void
+    {
+        $user = AdminFactory::createOne();
+        $I->amLoggedInAs($user->object());
+        EspeceFactory::createOne();
+        ClientFactory::createOne();
+        $I->sendPOST('/api/animals', [
+            'name' => 'Donald',
+            'espece' => '/api/especes/1',
+            'client' => '/api/clients/2',
+            'birthdate' => '2021-01-01',
+        ]);
+        $I->seeResponseCodeIs(HttpCode::CREATED);
     }
 
     public function authenticatedClientCanPostAnimalForHim(ApiTester $I): void
