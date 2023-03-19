65534e019be134e73938549f96e095a034d468a6
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-03-19 14:41:18 +0100

    Test pour vérifier que seul un admin peut modifier un animal

--- tests/Api/Animal/AnimalPatchCest.php
+++ tests/Api/Animal/AnimalPatchCest.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace App\Tests\Api\Animal;
+
+use App\Factory\AdminFactory;
 use App\Factory\AnimalFactory;
 use App\Factory\ClientFactory;
 use App\Factory\EspeceFactory;
+use App\Factory\VeterinaireFactory;
 use App\Tests\Support\ApiTester;
 use Codeception\Util\HttpCode;
 
@@ -11,13 +14,56 @@ class AnimalPatchCest
 {
     public function anonymousCannotPatchAnimal(ApiTester $I): void
     {
-        $user = ClientFactory::createOne();
-        AnimalFactory::createOne(['name' => 'Rex', 'espece' => EspeceFactory::createOne(['name' => 'Chien']), 'client' => ClientFactory::createOne()]);
+        AnimalFactory::createOne(['name' => 'Matoufeu', 'espece' => EspeceFactory::createOne(['name' => 'Chien']), 'client' => ClientFactory::createOne()]);
         $I->sendPATCH('/api/animals/1', [
             'name' => 'Donald',
             'espece' => '/api/especes/1',
             'client' => '/api/clients/1',
         ]);
         $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
+    }
+
+    public function authenticatedVeterinaireCannotPatchAnimal(ApiTester $I): void
+    {
+        $user = VeterinaireFactory::createOne();
+        $I->amLoggedInAs($user->object());
+        AnimalFactory::createOne(['name' => 'Matoufeu', 'espece' => EspeceFactory::createOne(['name' => 'Chien']), 'client' => ClientFactory::createOne()]);
+        $I->sendPATCH('/api/animals/1', [
+            'name' => 'Donald',
+            'espece' => '/api/especes/1',
+            'client' => '/api/clients/1',
+        ]);
+        $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
+    }
+
+    public function authenticatedAdminCanPatchAnimal(ApiTester $I): void
+    {
+        $user = AdminFactory::createOne();
+        $I->amLoggedInAs($user->object());
+        EspeceFactory::createOne();
+        ClientFactory::createOne();
+        AnimalFactory::createOne(['name' => 'Matoufeu', 'espece' => EspeceFactory::createOne(['name' => 'Chien']), 'client' => ClientFactory::createOne()]);
+        $I->sendPATCH('/api/animals/1', [
+            'name' => 'Donald',
+            'espece' => '/api/especes/1',
+            'client' => '/api/clients/2',
+            'birthdate' => '2021-01-01',
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+    }
+
+    public function authenticatedClientCannotPatchAnimal(ApiTester $I): void
+    {
+        $user = ClientFactory::createOne();
+        EspeceFactory::createOne();
+        AnimalFactory::createOne(['name' => 'Matoufeu', 'espece' => EspeceFactory::createOne(['name' => 'Chien']), 'client' => ClientFactory::createOne()]);
+        $I->amLoggedInAs($user->object());
+        $I->sendPATCH('/api/animals/1', [
+            'name' => 'Donald',
+            'espece' => '/api/especes/1',
+            'client' => '/api/clients/1',
+            'birthdate' => '2021-01-01',
+        ]);
+        $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
     }
 }
