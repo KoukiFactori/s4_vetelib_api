bed0595769fb94c357a368c97541bf74b826c965
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-03-20 14:15:35 +0100

    Ajout d'une sécurité Pour que les vétérinaires ne puissent récupérer que leur propre animaux

--- src/Controller/GetAllAnimalOfVeterinaireController.php
+++ src/Controller/GetAllAnimalOfVeterinaireController.php
@@ -9,9 +9,16 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class GetAllAnimalOfVeterinaireController extends AbstractController
 {
-    public function __construct(private AnimalRepository $er, private VeterinaireRepository $vr){}
+    public function __construct(private AnimalRepository $er, private VeterinaireRepository $vr)
+    {
+    }
+
     public function __invoke(int $id): array
     {
+        if ($this->getUser()->getId() != $id) {
+            throw new AccessDeniedException("Vous n'avez pas accès aux animaux de ce vétérinaire");
+        }
+
         return $this->er->getAllAnimalByVeterinaire($this->vr->find($id));
     }
 }
