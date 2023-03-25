5cc424bb24ebf6a5b932bb43159f863a399bd06c
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-03-25 21:59:45 +0100

    Ajout validator Validator qui verifie qu'un event ne peut pas en chevaucher un autre

---
+++ src/Validator/EventAtTheSameTimeValidator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Validator;
+
+use App\Repository\VeterinaireRepository;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class EventAtTheSameTimeValidator extends ConstraintValidator
+{   
+    
+    public function __construct(Private VeterinaireRepository $vr){}
+    public function validate($value, Constraint $constraint)
+    {
+    /* @var App\Validator\EventAtTheSameTime $constraint */
+
+    if (null === $value || '' === $value) {
+        return;
+    }
+
+    // TODO: implement the validation here
+    if (null != $this->vr->findEventByStartingTime($value)) {
+        $this->context->buildViolation($constraint->message)
+            ->setParameter('{{ value }}', $value)
+            ->addViolation();
+    }
+}
+}
