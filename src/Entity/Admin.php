1510b170ae4cd7d0688db63c643f7e4c0902bb71
Teia <nicolas.mossmann@etudiant.univ-reims.fr>
2023-03-26 06:23:10 +0200

    Remove APIResource tag to Admin

--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -2,13 +2,10 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiResource;
 use App\Repository\AdminRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AdminRepository::class)]
-#[ApiResource]
 class Admin extends User
 {
-    
 }
