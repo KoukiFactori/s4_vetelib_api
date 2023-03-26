7df338363ec7cd9677350ea65e83c6419bc89dfc
Teia <nicolas.mossmann@etudiant.univ-reims.fr>
2023-03-26 10:06:43 +0200

    Set admin as an empty-operation api resource

--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\AdminRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AdminRepository::class)]
+#[ApiResource(operations: [])]
 class Admin extends User
 {
 }
