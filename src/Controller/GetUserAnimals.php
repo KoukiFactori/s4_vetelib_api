eed7a230d868c7e59bb7d063facb38e3dee9a8e3
Teia <nicolas.mossmann@etudiant.univ-reims.fr>
2023-03-26 10:55:27 +0200

    Create controller for fetching all client animals

---
+++ src/Controller/GetUserAnimals.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Client;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class GetUserAnimals extends AbstractController
+{
+    public function __invoke()
+    {
+        #Client should be connected to pass security restriction and access this controller
+        
+        /**
+         * @var Client $client
+         */
+        $client = $this->getUser();
+
+        return $client->getAnimals();
+    }
+}
