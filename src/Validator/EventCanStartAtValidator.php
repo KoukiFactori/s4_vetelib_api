b40202a693292bd1571635f187332be286f900d1
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-03-26 15:41:49 +0200

    Correction Validator
    Correction d'un bug qui faisais que le validator ne fonctionnais pas

--- src/Validator/EventCanStartAtValidator.php
+++ src/Validator/EventCanStartAtValidator.php
@@ -12,7 +12,7 @@ class EventCanStartAtValidator extends ConstraintValidator
         /* @var App\Validator\EventCanStartAt $constraint */
 
         // TODO: implement the validation here
-        if (00 != intval($value->format('i')) or 30 != intval($value->format('i'))) {
+        if (00 != intval($value->format('i')) && 30 != intval($value->format('i'))) {
             $this->context->buildViolation($constraint->message)
                 ->setParameter('{{ value }}', $value->format('i'))
                 ->addViolation();
