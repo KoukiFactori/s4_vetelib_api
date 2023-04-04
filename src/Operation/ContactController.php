64adf3b62bffc27e7a8e3805e97ff5dac69a9e7d
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-04-04 21:45:49 +0200

    Ajout controller pour les mails
    Ajout du controller pour l'envoie de mail nescessite encore la securité

---
+++ src/Operation/ContactController.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Operation;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+
+class ContactController extends AbstractController
+{
+/**
+ * @Route("/contact", name="contact", methods={"POST"})
+ */
+    public function contact(Request $request, MailerInterface $mailer): Response
+    {
+        $email = $request->get('email');
+        $name = $request->get('lastname');
+        $firstname = $request->get('firstname');
+        $object = $request->get('message');
+        $title = $request->get('title');
+
+        $emailMessage = (new Email())
+            ->from($email)
+            ->to('simon@simon511000.fr ')
+            ->subject($title)
+            ->text(sprintf("From: %s (%s)\n\n%s", $name,$firstname, $object));
+
+        $mailer->send($emailMessage);
+
+        return new Response(null, Response::HTTP_CREATED);
+    }
+}
