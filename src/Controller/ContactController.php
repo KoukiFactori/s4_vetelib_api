a0ac6397a5b9c8cbb323f9024890d6d700255adb
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-04-05 14:56:35 +0200

    Controller contact
    Correction bug

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -9,12 +9,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
-use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'contact', methods: ['POST'])]
+    #[Route('/contact', name: 'contact')]
     public function contact(Request $request, MailerInterface $mailer): Response
     {   
         $email = $request->get('email');
