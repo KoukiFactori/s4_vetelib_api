3b4d60b4d6542116f05be9792af7a724a1fa7ee2
Nicolas Mossmann <nicolas.mossmann@etudiant.univ-reims.fr>
2023-04-07 08:02:08 +0200

    feat(veterinaire): Create route for clients collection

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -5,7 +5,8 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\OpenApi\Model;
-use App\Controller\GetUserAnimals;
+use App\Controller\GetAllClientOfVeterinaireController;
+use App\Controller\GetUserAnimalsController;
 use App\Repository\ClientRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -16,12 +17,43 @@ use Doctrine\ORM\Mapping as ORM;
     operations: [
         new GetCollection(
             uriTemplate: '/me/animals',
-            controller: GetUserAnimals::class,
+            controller: GetUserAnimalsController::class,
             security: 'is_granted("ROLE_CLIENT")',
             normalizationContext: ['animal:read:collection'],
             openapi: new Model\Operation(
                 summary: 'Get all animals from the current user',
                 description: 'Allow the current connected user to get all the animals they have',
+            )
+        ),
+    ]
+)]
+#[ApiResource(
+    uriTemplate: '/veterinaire/{id}/clients',
+    security: 'is_granted("ROLE_ADMIN") or is_granted("ROLE_VETERINAIRE")',
+    operations: [
+        new GetCollection(
+            controller: GetAllClientOfVeterinaireController::class,
+            openapi: new Model\Operation(
+                tags: ['Veterinaire'],
+                summary: 'Retrieves all clients related to a veterinaire',
+                description: 'Returns an array of all clients related to a veterinaire, clients who have an animal with an event with said veterinaire',
+                responses: [
+                    '200' => [
+                        'description' => 'Success',
+                    ],
+                    '401' => [
+                        'description' => 'Forbidden',
+                    ],
+                    '403' => [
+                        'description' => 'Unauthorized',
+                    ],
+                    '404' => [
+                        'description' => 'Veterinaire not found',
+                    ],
+                    '500' => [
+                        'description' => 'Server error',
+                    ],
+                ]
             )
         ),
     ]
