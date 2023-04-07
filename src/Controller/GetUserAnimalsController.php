7172989d307c66c14095476b8c8f1a16e8945898
Nicolas Mossmann <nicolas.mossmann@etudiant.univ-reims.fr>
2023-04-07 12:43:05 +0200

    Configure security for controller

--- src/Controller/GetUserAnimalsController.php
+++ src/Controller/GetUserAnimalsController.php
@@ -4,13 +4,22 @@ namespace App\Controller;
 
 use App\Entity\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class GetUserAnimalsController extends AbstractController
 {
+    public function __construct(
+        private Security $security
+    ) {
+    }
+
     public function __invoke()
     {
-        // Client should be connected to pass security restriction and access this controller
+        if (!$this->security->isGranted('ROLE_CLIENT')) {
+            return [];
+        }
 
+        // Client should be connected to pass security restriction and access this controller
         /**
          * @var Client $client
          */
